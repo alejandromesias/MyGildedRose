9eacf890872c14b82b10dcc73af6993f62d8aa39
AlejandroM <alejandromesiasu@gmail.com>
2026-08-27 18:39:50 -0500

    First refactor, removes some nested ifs

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,6 +3,7 @@ package com.gildedrose;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class GildedRoseTest {
 
