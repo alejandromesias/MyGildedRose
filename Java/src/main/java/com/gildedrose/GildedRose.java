f9844c38f301d3004bccd87422def154a259628d
AlejandroM <alejandromesiasu@gmail.com>
2026-08-27 23:37:01 -0500

    Tyding up

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,15 +1,16 @@
 package com.gildedrose;
 import java.util.HashMap;
+import java.util.Map;
 
 class GildedRose {
     Item[] items;
-    HashMap<String, ItemRule> ruleContainer = new HashMap<>();
+    private final Map<String, ItemRule> ruleContainer = new HashMap<>();
 
     public GildedRose(Item[] items) {
         this.items = items;
-        this.ruleContainer.put("Sulfuras, Hand of Ragnaros",new SulfuraRule());
-        this.ruleContainer.put("Aged Brie",new AgedBrieRule());
-        this.ruleContainer.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassesRule());
+        ruleContainer.put("Sulfuras, Hand of Ragnaros",new SulfuraRule());
+        ruleContainer.put("Aged Brie",new AgedBrieRule());
+        ruleContainer.put("Backstage passes to a TAFKAL80ETC concert", new BackstagePassesRule());
     }
     public void updateQuality() {
         for (Item item : items) {
